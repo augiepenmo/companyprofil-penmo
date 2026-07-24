ee653883c5074078c31d406c1b5040656cf62872
Augie <augiepenmo@gmail.com>
2026-07-24 11:07:19 +0700

    projectweb

---
+++ src/main.js
@@ -0,0 +1,68 @@
+import "./style.css";
+
+// Import Swiper
+import Swiper from "swiper";
+import { Autoplay, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+// Inisialisasi Swiper
+new Swiper(".menuSwiper", {
+    modules: [Autoplay, Pagination],
+
+    loop: true,
+
+    // Tambahkan ini
+    speed: 500,
+
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    spaceBetween: 20,
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+// ================= MENU MOBILE =================
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+});
+
+// ================= NAVBAR =================
+window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("navbar-scroll");
+    } else {
+        navbar.classList.remove("navbar-scroll");
+    }
+});
+
+// ================= CONTACT =================
+document.getElementById("contactForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Terima kasih! Pesan Anda berhasil dikirim.");
+});
